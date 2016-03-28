3033780ebc33c2845817484a2cef516181b2e6dc
Twinkle Chawla <twinkle1chawla@gmail.com>
2016-03-28 21:50:03 +0530

    encrypted in DB

--- api/models/User.js
+++ api/models/User.js
@@ -51,7 +51,7 @@ module.exports = {
   	}
   	require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword){
   		if (err) return next(err);
-  		values.encryptedPassword = encryptedPassword;
+  		values.password = encryptedPassword;
   		next();
   	});
   }
